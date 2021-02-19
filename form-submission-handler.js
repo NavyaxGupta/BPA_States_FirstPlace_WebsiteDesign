0d286ee8226695e57b5ee73b080a93d674245d13
infinitysoftwaredev <75912590+infinitysoftwaredev@users.noreply.github.com>
2021-02-18 21:35:54 -0500

    Update form-submission-handler.js

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -80,6 +80,7 @@
                 }
             }
         };
+
         // url encode form data for sending as post data
         var encoded = Object.keys(data).map(function(k) {
             return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
@@ -103,3 +104,25 @@
         }
     }
 })();
+
+
+
+//Get the button:
+mybutton = document.getElementById("top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
