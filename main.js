f5641938c601c54fab43cc1012a3f6a97c91f0d4
infinitysoftwaredev <75912590+infinitysoftwaredev@users.noreply.github.com>
2021-03-23 09:19:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ showDivs(slideIndex);
 var myIndex = 0;
 carousel();
 
-function plusDivs(n) {
+// Next/previous controls
+function plusSlides(n) {
     showDivs(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+    showDivs(slideIndex = n);
+}
+
+//Operated Slideshow
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
@@ -18,19 +26,28 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block";
     myIndex=n;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex-1].className += " active";
 }
 
+//Automatic slideshow
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
-
+    var dots = document.getElementsByClassName("dot");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
-        x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000); // Change image every 3 seconds
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    x[myIndex-1].style.display = "block";
+    dots[myIndex-1].className += " active";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
 
